97e2080b476549d817598a8ecb5dc8ffe1d0e112
Samantha S. <sevensasylum@gmail.com>
2024-10-25 12:29:44 -0400

    Update notes_python.py

--- notes_python.py
+++ notes_python.py
@@ -6,6 +6,7 @@ elif <condition 2 is true>
     <do B>
 else
     <do C>
+    
 # Nested if statements
 if <condition 1 is true>
     <do A>
@@ -13,6 +14,7 @@ if <condition 1 is true>
         <do B>
         if <condition 3 is true>
             <do C>
+            
 # Multiple if statements
 if <condition 1 is true>
     <do A>
