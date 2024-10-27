7ae4501db17cf1b6ee577e0eb8221f82373b0dd7
Seven <sevensasylum@gmail.com>
2024-10-27 11:07:45 -0400

    Updated Treasure Island

--- mini_projects/treasure.py
+++ mini_projects/treasure.py
@@ -27,16 +27,20 @@ direction = input('You\'re at a crossroad, Do you want to go Left or Right?'
                   ' Type "left" or "right"\n').lower()
 
 #create conditional based on choices
-if direction == "left":
+if direction == "left": #If the user types left then do this
     choice1 = input('You\'ve come to a lake of roses known as "El Agua de Amour."'
                     ' There is an island with a castle in the middle of the lake.'
                     ' Type "wait" to wait for the boat.'
                     ' Type "swim" to swim across.\n').lower()
-    if choice1 == "wait":
+    #this is a nested statement within in the if direction == left because left leads to the next question
+    #whereas if user picks right, game is over. choice 1 == wait is also nested leads to next question
+    #which door? which is nested inside choice2 == wait
+    if choice1 == "wait": 
         choice2 = input('You arrive at the castle unharmed.'
                         ' There is the castle entrance with 3 doors. One red,'
                         ' one yellow and one blue. '
                         ' Which color do you choose?\n').lower()
+        #this is nested inside choice1 == wait  which also inside of choice2 == pick a color
         if choice2 == "red":
             print("It's a room full of fire, Game over!")
         elif choice2 == "yellow":
@@ -47,5 +51,9 @@ if direction == "left":
             print("You chose a door that doesn't exist. Game Over!")
     else:
         print("You got attacked by an angry big. Game over!")
+
+        #this else statement is for if the user picks "Right" then right off the else states game over
+        #as you can see the indentation in the beginning of lines and usually you can tell
+        #if the code is nested of not
 else:
     print("You fell in to a hole and swallowed up. Game Over.")
