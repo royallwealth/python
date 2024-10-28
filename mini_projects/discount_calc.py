7542fb00c19c877be16baac2eab7642a5017ba21
Seven <sevensasylum@gmail.com>
2024-10-28 18:23:51 -0400

    discount calculator

---
+++ mini_projects/discount_calc.py
@@ -0,0 +1,16 @@
+# DISCOUNT CALCULATOR
+# Samantha S.
+
+print("""
+   88             88 88                        
+         88             88 88                        
+         88             88 88                        
+ ,adPPYb,88  ,adPPYba,  88 88 ,adPPYYba, 8b,dPPYba,  
+a8"    `Y88 a8"     "8a 88 88 ""     `Y8 88P'   "Y8  
+8b       88 8b       d8 88 88 ,adPPPPP88 88          
+"8a,   ,d88 "8a,   ,a8" 88 88 88,    ,88 88          
+ `"8bbdP"Y8  `"YbbdP"'  88 88 `"8bbdP"Y8 88          
+                                                
+      """)
+
+
