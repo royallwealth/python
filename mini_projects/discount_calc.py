9a88b609f3c90ab87f8f64485b3aaae662d9d937
Seven <sevensasylum@gmail.com>
2024-10-28 19:24:12 -0400

    discount calc updated

--- mini_projects/discount_calc.py
+++ mini_projects/discount_calc.py
@@ -1,4 +1,4 @@
-# DISCOUNT CALCULATOR
+# DISCOUNT CALCULATOR 🧮
 # Samantha S.
 
 print("""
@@ -12,5 +12,21 @@ a8"    `Y88 a8"     "8a 88 88 ""     `Y8 88P'   "Y8
  `"8bbdP"Y8  `"YbbdP"'  88 88 `"8bbdP"Y8 88          
                                                 
       """)
+
+print("Welcome to the in-store discount calculator!🚪")
+
+# user inputs
+price = float(input("What is the total bill?\n$"))
+discount = int(input("Which coupon would you like to apply? 5%, 10%, or 20%:\nCoupon: "))
+
+# stored calculation discount % divided out of 100% times the price gives the discount amount
+# the final amount is the price - the discount
+
+discount_amount = round(discount / 100 * price)
+final_amount = round(price - discount_amount, 2)
+
+# print the final calculations
+print(f"Discount: ${discount_amount}")
+print(f"Final Bill: ${final_amount}")
 
 
