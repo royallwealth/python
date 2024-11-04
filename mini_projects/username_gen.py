ff4bae3d745b8e4757db56c15267500c25cd0874
Seven <sevensasylum@gmail.com>
2024-11-04 01:12:48 -0500

    working

--- mini_projects/username_gen.py
+++ mini_projects/username_gen.py
@@ -1,7 +1,32 @@
 # PYTHON USERNAME GENERATOR 🔑
 # Samantha S.
 
-# Creating lists
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+import random
+
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+# user inputs
+
+print("Welcome to the python username generator 😁 ")
+users_name = (input("What is the user's name?\n"))
+choice = (input("Would you like numbers included? Type Yes or No\n"))
+num = int(input("How many numbers would you like to include?\n"))
+theme = (input("What is your favorite season?\n"))
+
+username = []
+
+username.extend(random.choice(users_name))
+username.extend(theme)
+   
+if choice.lower() == "yes":
+    for x in range(0, num):
+        username.append(random.choice(numbers))
+else:
+    print('Thank you :)')
+
+print(username)    
+new_user = ''.join(username)
+print(f"Generated Username: {new_user}")
+
+
 
--- pract_assign.py
+++ pract_assign.py
@@ -80,3 +80,33 @@ for numbers in range(1, 101):
     else:
         print(numbers)
 """
+
+
+
+"""
+#using something like psuedo code
+#remove what in parenthesis
+
+we need a max variable 
+
+max = 0
+
+for (every) score in student_scores:
+    #please do this:
+    if (the) score (is) > max:
+    (then) the max need to = score
+
+"""
+# student_scores = [8, 65, 89, 86, 55, 91, 64, 89]
+# max = 0
+# for score in student_scores:
+#     if score > max:
+#         max = score
+# print(max)
+
+# print(total_sum)
+
+sum = 0
+for total in range(1, 101):
+    sum += total
+print(f"The Total Sum: {sum}")
